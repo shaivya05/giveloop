eee0126ab002326e03bc8a8e49d0691d34357250
shaivya05 <130532388+shaivya05@users.noreply.github.com>
2026-05-11 22:34:50 +0530

    Fix:updated cors

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,13 @@ dotenv.config()
 
 const app = express()
 
-app.use(cors())
+app.use(cors({
+  origin: [
+    'http://localhost:3000',
+    'https://giveloop-six.vercel.app'
+  ],
+  credentials: true
+}))
 app.use(express.json())
 
 const authRoutes = require('./routes/authRoutes')
